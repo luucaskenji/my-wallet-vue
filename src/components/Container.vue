3a66c491bebd868eaa232795c84ddc7b13c9c4aa
Lucas Murakami <lkenjim98@gmail.com>
2021-04-03 03:01:16 -0300

    Login page layout

---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Container',
+});
+</script>
+
+<style scoped>
+  div {
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
---
+++ src/views/Auth.vue
@@ -0,0 +1,60 @@
+<template>
+  <Container v-if="showLoginForm">
+    <h1>My Wallet</h1>
+    <form>
+      <input type='email' placeholder='E-mail'>
+      <input type='password' placeholder='Senha'>
+
+      <button type="submit">Entrar</button>
+    </form>
+  </Container>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+import Container from '../components/Container.vue';
+
+export default defineComponent({
+  name: 'Auth',
+  components: {
+    Container,
+  },
+  data() {
+    return {
+      showLoginForm: true,
+    };
+  },
+});
+</script>
+
+<style scoped>
+  h1 {
+    color: white;
+    font-family: 'Teko', sans-serif;
+    font-size: 1.8rem;
+    margin-bottom: 22px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 220px;
+  }
+
+  input {
+    width: 100%;
+    height: 60px;
+    margin-bottom: 16px;
+    font-size: 1rem;
+    border-radius: 6px;
+    padding-left: 10px;
+  }
+
+  button {
+    color: white;
+    height: 60px;
+    background: #A86CD6;
+    border-radius: 6px;
+    font-size: 1rem;
+  }
+</style>
