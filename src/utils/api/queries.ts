8a6960a1b7c4ecb9c2736de0a569b148f24e51b7
Lucas Murakami <lkenjim98@gmail.com>
2021-05-28 18:54:29 -0300

    add queries types

--- src/utils/api/queries.ts
+++ src/utils/api/queries.ts
@@ -1,8 +1,16 @@
 /* eslint-disable import/prefer-default-export */
 import { gql } from '@apollo/client';
 
+export type UserInput = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
 export const createUserAccount = gql`
-  mutation createUser(input: createUserInput!) {
+  mutation createUser(input: UserInput!) {
     createUser(input: $input) {
       id
     }
