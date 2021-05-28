6ce0e6a7285a28767e81cc347fc8dd3adb7afe40
Lucas Murakami <lkenjim98@gmail.com>
2021-05-28 18:21:49 -0300

    add user state in vuex store

--- src/store/modules/user/state.ts
+++ src/store/modules/user/state.ts
@@ -1,1 +1,18 @@
-interface UserState {}
+interface UserInfo {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+interface UserState {
+  user: UserInfo | null;
+  accessToken: string;
+}
+
+const state: UserState = {
+  user: null,
+  accessToken: '',
+};
+
+export default state;
