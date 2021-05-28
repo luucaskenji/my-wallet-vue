a11cbc7694fa7d5978ebf28abfd47079891a52bd
Lucas Murakami <lkenjim98@gmail.com>
2021-05-28 18:45:31 -0300

    create functions to make queries and mutations to the API

--- src/utils/api/client.ts
+++ src/utils/api/client.ts
@@ -1,6 +1,33 @@
-import { ApolloClient, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
+/* eslint-disable @typescript-eslint/no-explicit-any */ // Needed for queries returns
+
+import {
+  ApolloClient,
+  DocumentNode,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client';
 
 const client = new ApolloClient<NormalizedCacheObject>({
   cache: new InMemoryCache(),
   uri: 'localhost:4000/graphql',
 });
+
+export async function APIQuery(
+  query: DocumentNode,
+  variables?: { [key: string]: any },
+): Promise<any> {
+  return client.query({
+    query,
+    variables,
+  });
+}
+
+export async function APIMutation(
+  mutation: DocumentNode,
+  variables?: { [key: string]: any },
+): Promise<any> {
+  return client.mutate({
+    mutation,
+    variables,
+  });
+}
