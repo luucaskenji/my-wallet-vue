8536cd1c6b6d537eb3661c08635ca0391602244c
Lucas Murakami <lkenjim98@gmail.com>
2021-05-29 16:26:42 -0300

    user vuex getter

--- src/store/modules/user/getters.ts
+++ src/store/modules/user/getters.ts
@@ -1,6 +1,13 @@
 import { GetterTree } from 'vuex';
 
 import { RootState } from '@/store/rootState';
+import { UserModel } from '@/models/UserModel';
 import { UserState } from './state';
 
-export const getters: GetterTree<UserState, RootState> = {};
+export const getters: GetterTree<UserState, RootState> = {
+  user: (state): UserModel | null => (
+    state.user
+      ? new UserModel(state.user, state.accessToken)
+      : null
+  ),
+};
