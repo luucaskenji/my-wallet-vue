1b3926aef10153cda32db829e40481f00f361775
Lucas Murakami <lkenjim98@gmail.com>
2021-05-29 19:08:01 -0300

    Dashboard view skeleton

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,16 +1,33 @@
 <template>
-  <div>Dashboard page</div>
+  <Container>
+    <header>
+      <div>
+        <span>{{ `${user.firstName} ${user.lastName}` }}</span>
+      </div>
+    </header>
+  </Container>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
 
+import { Container } from '@/components';
+import { UserModel } from '@/models/UserModel';
+
 interface Data {}
 
 export default defineComponent({
   name: 'Dashboard',
+  components: {
+    Container,
+  },
   data(): Data {
     return {};
+  },
+  computed: {
+    user(): UserModel {
+      return this.$store.getters.user || {};
+    },
   },
 });
 </script>
