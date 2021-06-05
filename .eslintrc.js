fc8df372a27d0613012395c4a70e638544d44c29
Lucas Murakami <lkenjim98@gmail.com>
2021-06-05 18:52:36 -0300

    move API endpoint to env variable

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     'no-shadow': 'off',
     'import/prefer-default-export': 'off',
     'lines-between-class-members': 'off'
