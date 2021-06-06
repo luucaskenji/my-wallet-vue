704c556e3351a86fe438ec09510d6598d0626bd8
Lucas Murakami <lkenjim98@gmail.com>
2021-06-06 16:11:01 -0300

    add inputs and button to Transaction view

--- src/views/Transaction.vue
+++ src/views/Transaction.vue
@@ -1,15 +1,22 @@
 <template>
-  <Container justifyContent='flex-start' alignItems='stretch'>
-    <div class='container'>
-      <Input type='text' />
-    </div>
+  <Container justifyContent='flex-start'>
+    <form>
+      <div class="title">
+        {{ formData.title[transactionType] }}
+      </div>
+
+      <Input type='text' placeholder='Valor'/>
+      <Input type='text' placeholder='Descrição'/>
+
+      <Button type='submit' :innerText='formData.buttonText[transactionType]'/>
+    </form>
   </Container>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
 
-import { Container, Input } from '@/components';
+import { Container, Input, Button } from '@/components';
 
 interface Data {}
 
@@ -17,15 +24,39 @@ export default defineComponent({
   components: {
     Container,
     Input,
+    Button,
   },
   data(): Data {
     return {};
+  },
+  computed: {
+    transactionType(): string {
+      return this.$route.params.transactionType as string;
+    },
+    formData(): { [key: string]: { [key: string]: string } } {
+      return {
+        title: {
+          entrada: 'Nova entrada',
+          saida: 'Nova saída',
+        },
+        buttonText: {
+          entrada: 'Salvar entrada',
+          saida: 'Salvar saída',
+        },
+      };
+    },
   },
 });
 </script>
 
 <style scoped>
-.container {
+form {
   padding: 0 20px;
+}
+
+.title {
+  font-size: 1.4rem;
+  color: white;
+  margin: 15px 0;
 }
 </style>
