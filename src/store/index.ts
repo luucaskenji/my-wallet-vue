a9b3aa07869de4fde59a0a711089b8537bb3b59c
Lucas Murakami <lkenjim98@gmail.com>
2021-06-05 21:50:40 -0300

    getFinances vuex action config

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
 
 import user from './modules/user';
+import finances from './modules/finances';
 
 export default createStore({
   state: {},
   modules: {
     user,
+    finances,
   },
 });
--- src/store/modules/finances/actions.ts
+++ src/store/modules/finances/actions.ts
@@ -5,8 +5,16 @@ import { getFinances } from '@/utils/api/queries';
 import { APIQuery } from '@/utils/api/client';
 import { FinanceState } from './state';
 
+export enum FinanceStoreActions {
+  GET_FINANCES = 'GET_FINANCES',
+}
+
 export const actions: ActionTree<FinanceState, RootState> = {
   async GET_FINANCES() {
-    return APIQuery(getFinances);
+    const response = await APIQuery(getFinances) as any;
+
+    const finances = response.data.getFinances;
+
+    console.log(finances);
   },
 };
