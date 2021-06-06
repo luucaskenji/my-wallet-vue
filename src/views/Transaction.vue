eb8865f3bdb04196c7a5353065ca17350bc54a49
Lucas Murakami <lkenjim98@gmail.com>
2021-06-05 21:56:11 -0300

    create Transaction view skeleton

---
+++ src/views/Transaction.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+interface Data {}
+
+export default defineComponent({
+  data(): Data {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
