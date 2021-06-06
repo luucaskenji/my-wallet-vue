29b1138a26788e36ed73a0b6283677f3a2865474
Lucas Murakami <lkenjim98@gmail.com>
2021-06-05 21:50:04 -0300

    insert authorization headers in graphql queries

--- src/utils/api/client.ts
+++ src/utils/api/client.ts
@@ -1,25 +1,36 @@
-// Needed for queries returns
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 import {
   ApolloClient,
   DocumentNode,
   InMemoryCache,
   NormalizedCacheObject,
-  HttpLink,
+  createHttpLink,
 } from '@apollo/client/core';
+import { setContext } from '@apollo/client/link/context';
 import fetch from 'unfetch';
 
-const client = new ApolloClient<NormalizedCacheObject>({
-  cache: new InMemoryCache(),
-  link: new HttpLink({ uri: 'http://localhost:4000/graphql', fetch: fetch as any }),
-});
+function getApolloClient(): ApolloClient<NormalizedCacheObject> {
+  const httpLink = createHttpLink({ uri: process.env.VUE_APP_API_ENDPOINT, fetch: fetch as any });
+
+  const authLink = setContext((_, { headers }) => ({
+    headers: {
+      ...headers,
+      authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+    },
+  }));
+
+  return new ApolloClient<NormalizedCacheObject>({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+}
 
 export async function APIQuery(
   query: DocumentNode,
   variables?: { [key: string]: any },
 ): Promise<any> {
-  return client.query({
+  const a = getApolloClient();
+
+  return a.query({
     query,
     variables,
   });
@@ -29,7 +40,7 @@ export async function APIMutation(
   mutation: DocumentNode,
   variables?: { [key: string]: any },
 ): Promise<any> {
-  return client.mutate({
+  return getApolloClient().mutate({
     mutation,
     variables,
   });
